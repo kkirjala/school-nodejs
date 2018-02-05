2408120329fa859acf37a9b5c757520dc7842a0b
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-05 16:56:27 +0200

    Added CORS middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 app.use(bodyParser.json())
+
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :body :res[content-length] - :response-time ms'))
