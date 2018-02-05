007b43420ca7c44cc250fc4a03af512cb3d4c371
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-05 14:22:56 +0200

    Displaying of a static list of persons (task 3.1) and summary info (task 3.2)

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "phoneNumber": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Martti Tienari",
+      "phoneNumber": "040-123456",
+      "id": 2
+    },
+    {
+      "name": "Arto Järvinen",
+      "phoneNumber": "040-123456",
+      "id": 3
+    },
+    {
+      "name": "Lea Kutvonen",
+      "phoneNumber": "040-123456",
+      "id": 4
+    }
+  ]
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello.</h1><div>Please use some other endpoint.</div>')
+})
+
+app.get('/info', (req, res) => {   
+    res.send(`puhelinluettelossa ${persons.length} henkilön tiedot.<br/>${new Date()}`)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)    
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server started using TCP port ${PORT}`)
+})
