1ac95701c952f0679e3f05d90c8d53d9f10facd1
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-05 16:35:50 +0200

    Enable logging of request body contents (task 3.8)

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const app = express()
 app.use(bodyParser.json())
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :body :res[content-length] - :response-time ms'))
 
 
 let persons = [
