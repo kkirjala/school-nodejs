9048384e32e4ca140fedb5baaa5d2b1d15563be0
Kalle Kirjalainen <kalle.kirjalainen@gmail.com>
2018-02-06 16:10:05 +0200

    Added suppot for dotenv, removed earlier dbconfig.js based solution

--- models/person.js
+++ models/person.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
-const env = process.env.NODE_ENV || 'development';
-const config = require('../dbconfig')[env];
 
-const url = `mongodb://${config.database.username}:${config.database.password}@${config.database.host}:${config.database.port}/${config.database.db}`
+if ( process.env.NODE_ENV !== 'production' ) {
+    require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
